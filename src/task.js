b59adf8a0289cb9c4a48a6f691014f72812d84cf
Camila Merino <camperkkala@gmail.com>
2024-08-16 17:30:42 -0600

    Proyecto Terminado

--- src/task.js
+++ src/task.js
@@ -13,3 +13,20 @@ completed: false,
 tasks.push(newTask);
 localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+// Función para eliminar una tarea
+export const deleteTask = (id) => {
+tasks = tasks.filter((task) => task.id !== parseInt(id));
+localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// Función para actualizar una tarea
+export const toggleTask = (id) => {
+tasks = tasks.map((task) => {
+if(task.id === parseInt(id)) {
+task.completed = !task.completed;
+}
+return task;
+});
+localStorage.setItem("tasks", JSON.stringify(tasks));
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode:'development', 
     entry: './src/index.js', // Punto de entrada de la aplicación
     output: {
         filename: 'bundle.js', // Nombre del archivo de salida
@@ -10,13 +11,13 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/, // Regex para identificar archivos CSS
-                use: ['style-loader, ', 'css-loader'], //Loaders para procesar archivos CSS
+                use: ['style-loader', 'css-loader'], //Loaders para procesar archivos CSS
             },
             {
                 test: /\.js$/, // Regex para identifcar archivos JS
                 exclude: /node_modules/, // Excluir carpeta node_modules
                 use: {
-                    loader 'babel-loader', // Loader para llevar JS moderno a JS antiguo para que sea compatible con todos los navegadores
+                    loader: 'babel-loader', // Loader para llevar JS moderno a JS antiguo para que sea compatible con todos los navegadores
                     options: {
                         presets: ['@babel/preset-env'],
                     },
@@ -26,7 +27,9 @@ module.exports = {
     },
     devtool: 'source-map', // Generar mapas de código para facilitar la depuración
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'), // Carpeta desde la cual el servidor toma los archivos
+        static: {
+directory: path.resolve(__dirname, 'dist'),
+        },
         compress: true, // Habilitar la compresión gzip
         port: 9000, // Puerto de servidor de desarrollo
     },
